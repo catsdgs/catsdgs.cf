4f9afb3a2eaaa404c4921951151a61a95cbb2e74
D.J. Saderholm <therealcatsdgs@gmail.com>
2017-06-23 13:51:39 -0600

    afaf

--- loginjava.js
+++ loginjava.js
@@ -3,13 +3,15 @@
 		var pswd = document.getElementById('pwd');
 		// Incorrect Password: "catsdgs password" correct: var passcorrect = "cat5ist!";
 		var user = "catsdgs";
-		var pass = "catsdgs";
+
+		var pass = "username";
 		
 		if(username.value === user && pswd.value === pass) {
 			console.log('You are logged in!');
 		    window.open('testsazdf90dfsfsd09fsd9vasd88hhchxhjkzcjhcxch.html','_self', false);
-		}else{
-			alert("Invalid Username or password")
+		}
+		else{
+			alert("Invalid Username or Password")
 		}
 		
 		console.log(username.value);
