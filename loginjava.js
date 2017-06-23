57ad2cb2644128e43d3b66530177fdf5ced8d9a1
D.J. Saderholm <therealcatsdgs@gmail.com>
2017-06-23 14:13:28 -0600

    sdfsdfs

--- loginjava.js
+++ loginjava.js
@@ -1,19 +1,20 @@
 		function login() {
 	    var username = document.getElementById('username');
 		var pswd = document.getElementById('pwd');
-		// Incorrect Password: "catsdgs password" correct: var passcorrect = "cat5ist!";
-		var user = "catsdgs";
-
-		var pass = "username";
+// Incorrect Password: var AfterUserName correct: var passcorrect4all = "capdt5ist1lol5this5fake+";
 		
-		if(username.value === user && pswd.value === pass) {
+		var users = {};
+		
+		users["catsdgs"] = "Sherave4!";
+		users["user1"] = "user1";
+		users["user2"] = "user2";
+		
+		
+		if(users[username.value] === pswd.value){
 			console.log('You are logged in!');
-		    window.open('testsazdf90dfsfsd09fsd9vasd88hhchxhjkzcjhcxch.html','_self', false);
+			window.open('testsazdf90dfsfsd09fsd9vasd88hhchxhjkzcjhcxch.html','_self', false);
+		} else {
+			alert("Incorrect Username or Password")
 		}
-		else{
-			alert("Invalid Username or Password")
 		}
 		
-		console.log(username.value);
-		console.log(pswd.value);
-		}
