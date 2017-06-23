a36f619dd8099a92c0367a34870ca7e392f55972
D.J. Saderholm <therealcatsdgs@gmail.com>
2017-06-23 13:17:30 -0600

    dfgdfgdfg

--- loginjava.js
+++ loginjava.js
@@ -1,14 +1,15 @@
-		// Correct Password: cat5is5t!
 		function login() {
 	    var username = document.getElementById('username');
 		var pswd = document.getElementById('pwd');
-		// Incorrect Password: catsdgs password var passcorrect = "cat5ist!";
+		// Incorrect Password: "catsdgs password" correct: var passcorrect = "cat5ist!";
 		var user = "catsdgs";
 		var pass = "catsdgspassword";
 		
 		if(username.value === user && pswd.value === pass) {
 			console.log('You are logged in!');
 		    window.open('testsazdf90dfsfsd09fsd9vasd88hhchxhjkzcjhcxch.html','_self', false);
+		}else{
+			alert("Invalid Username or password")
 		}
 		
 		console.log(username.value);
